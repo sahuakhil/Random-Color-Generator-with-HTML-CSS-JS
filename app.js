723afb664c9b50d1f2dc11825d0f2f93fbf0cd66
Akhilesh <akhileshshah75@gmail.com>
2023-08-13 15:11:23 +0530

    Random color generate

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let bgColorsArray = ["#e75d7c", "#b16cef", "#53cca4", "#efc84d", "#628ef0", "#184b73", "#883e7f", "#ed048b"];
+let bgColorsArray = ["grey","green","red", "#53cca4", "#efc84d", "#628ef0", "#184b73", "#883e7f", "#ed048b"];
 
 let bgContainerelement = document.getElementById("bgContainer");
 let buttonElement = document.getElementById("button");
